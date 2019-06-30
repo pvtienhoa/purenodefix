e2a98def9d2ec3c299198d4ce3afb9b9bee8b677
pvtienhoa <pvtienhoa@gmail.com>
2019-07-01 00:16:48 +0700

    fix undisposed objects - 11th attemp

--- MDCapture/marketdata-client.ts
+++ MDCapture/marketdata-client.ts
@@ -228,7 +228,7 @@ export class MarketDataClient extends AsciiSession {
         if (this.isIdling) this.idleDuration += 200;
         else this.idleDuration = 0;
         this.isIdling = true;
-        if (this.idleDuration >= this.appConfig.FNoMsgResetTimeout * 1 * 1000) {
+        if (this.idleDuration >= this.appConfig.FNoMsgResetTimeout * 60 * 1000) {
             this.eventLog.info(`Client has been idle for ${this.appConfig.FNoMsgResetTimeout} minutes, Reconnecting`);
             this.logger.info(`Client has been idle for ${this.appConfig.FNoMsgResetTimeout} minutes, Reconnecting`);
             this.done();
